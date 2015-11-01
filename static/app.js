ace479d2a252b1776e67b77ac2a198894c19c3da
persidskiy <persidskiy@yandex-team.ru>
2015-11-02 00:05:03 +0300

    hide photo untill not loaded

--- static/app.js
+++ static/app.js
@@ -1,8 +1,39 @@
-[].forEach.call(document.querySelectorAll('img'), function(img) {
-	console.log('img.load');
-	var virtualImg = document.createElement('img');
-	
-	virtualImg.addEventListener('load', function () {
-		console.log('img.loaded');
-	})
+[].forEach.call(document.querySelectorAll('.photo'), function(photo) {
+	var img = photo.querySelector('.photo--image'),
+		virtualImg = document.createElement('img'),
+		isResponsive = false;
+
+	var onLoad = function (e) {
+		virtualImg.removeEventListener('load', onLoad);
+		virtualImg.removeEventListener('error', onLoad);
+		photo.className += ' photo-loaded';
+	}
+
+	virtualImg.addEventListener('load', onLoad);
+	virtualImg.addEventListener('error', onLoad);
+
+	if(img.parentNode.tagName.toLowerCase() === 'picture') {
+		var virtualParent = img.parentNode.cloneNode(true)
+		virtualParent.removeChild(virtualParent.querySelector('.photo--image'))
+		virtualParent.appendChild(virtualImg);
+		isResponsive = true;
+	}
+
+	if(img.srcset) {
+		virtualImg.srcset = img.srcset;
+		if(!isResponsive) {
+			document.createElement.appendChild(virtualImg);
+		}
+	} else if(img.src) {
+		virtualImg.src = img.src;
+	}
+
+
+	if(isResponsive && !window.HTMLPictureElement) {
+		if(window.picturefill){
+			window.picturefill({elements: [virtualImg]});
+		} else if(img.src) {
+			virtualImg.src = img.src;
+		}
+	}
 });
